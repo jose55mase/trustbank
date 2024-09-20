3090a51ae8c07b0c3cd2570580fae20041cfe042
joseluis.castaneda.cardona.contractor <joseluis.castaneda.cardona.contractor@bbva.com>
2024-09-20 18:05:15 -0500

    Desarrollo

--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/Index.java
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/Index.java
@@ -2,17 +2,14 @@ package com.bolsadeideas.springboot.backend.apirest.controllers;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.TransactionEntity;
 import com.bolsadeideas.springboot.backend.apirest.models.services.intefaces.ITransactionService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-
+@RequestMapping("/index")
 public class Index {
 
     @GetMapping()
