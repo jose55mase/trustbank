be810f049d7c9d14b939fbbe5acd8a3100cd8bf5
joseluis.castaneda.cardona.contractor <joseluis.castaneda.cardona.contractor@bbva.com>
2024-09-20 12:30:52 -0500

    Desarrollo

--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/TransactionConstructor.java
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/TransactionConstructor.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@CrossOrigin(origins = { "http://localhost:4200" })
+@CrossOrigin(origins = { "https://guardianstrustbank.com" })
 @RestController
 @RequestMapping("/api/transaction")
 public class TransactionConstructor {
--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/UserConstructor.java
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/UserConstructor.java
@@ -21,7 +21,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = { "http://localhost:4200" })
+@CrossOrigin(origins = { "https://guardianstrustbank.com" })
 @RestController
 @RequestMapping("/api/user")
 public class UserConstructor {
