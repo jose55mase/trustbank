7476f334fb6ab17a14d9ff6ba71dde7d176026cc
OJC04152 <jose.castaneda.ext@itau.co>
2025-12-09 20:55:03 -0500

    Configuracion de aplicacion inicial para prestamos

--- loans_receipt_app/build/web/flutter_service_worker.js
+++ loans_receipt_app/build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "01537e066cec5cf1def3caa0d80d307f",
+const RESOURCES = {"flutter_bootstrap.js": "05eb9670ae7bc18f8d6df6c8a7dd0b53",
 "version.json": "645866738483047b3eb8ea840c19ca8a",
 "index.html": "e798855e4b66d98f9ee98e006d5ce347",
 "/": "e798855e4b66d98f9ee98e006d5ce347",
-"main.dart.js": "ab38a273afd61122c26e7ecb410d4897",
+"main.dart.js": "c7fc0393d5a982d5e9e04f360d862369",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "01537e066cec5cf1def3caa0d80d307f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/fonts/MaterialIcons-Regular.otf": "b3424cba5e17f42fcd781049b4c9aeca",
+"assets/fonts/MaterialIcons-Regular.otf": "5f56591c3359fc399f6d7d5b3a9bef49",
 "canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
